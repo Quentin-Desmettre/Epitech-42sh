f68a6e0d4e69c3ab8f31e1873bc91e11b34cea27
Noa <noa.trachez@epitech.eu>
2022-05-24 15:52:24 +0200

    [+] Adding local and env variable when $ is used

--- src/parse_input/split_semicolon.c
+++ src/parse_input/split_semicolon.c
@@ -309,6 +309,71 @@ void delete_all_back_slash(char **str)
     }
 }
 
+char const *noa_better_get_field(char **env, char const *field, char spec)
+{
+    int f_len = my_strlen(field);
+
+    for (int i = 0; env[i]; i++)
+        if (my_strncmp(env[i], field, f_len) == 0 && env[i][f_len] == spec)
+            return env[i] + f_len + 1;
+    return NULL;
+}
+
+void get_variable(char **str, const char *source, int *len_var, int index_var)
+{
+    for (int i = index_var; source[i]; i++, (*len_var)++)
+        if (source[i] == ' ' ||
+        (i > 0 && source[i - 1] == ')') ||
+        source [i] == '$' || source[i] == '\0')
+            break;
+    (*str) = malloc(sizeof(char) * (*len_var + 1));
+    for (int i = 0; i < (*len_var) && source[index_var + i]; i++)
+        (*str)[i] = source[index_var + i];
+    (*str)[*len_var] = '\0';
+}
+
+void loop_to_replace(char **str, int *next_index, char **env, char spec)
+{
+    char *tmp = NULL;
+    char *str_to_cmp = NULL;
+    int len_to_replace = 0;
+
+    for (; (*str)[*next_index]; (*next_index)++)
+        if (*next_index && (*str)[*next_index - 1] == '$')
+            break;
+    get_variable(&str_to_cmp, (*str), &len_to_replace, *next_index);
+    tmp = (char *)noa_better_get_field(env, str_to_cmp, spec);
+    if (tmp) {
+        tmp = replace((*str), *next_index - 1, len_to_replace + 1, tmp);
+        free((*str));
+        (*str) = tmp;
+    }
+}
+
+void set_loop_to_replace(char **str, char **env, char spec)
+{
+    char *tmp = NULL;
+    int nbr_of_var = 0;
+    int next_index = 0;
+
+    for (int j = 0; (*str)[j]; j++)
+        nbr_of_var += (*str)[j] == '$' ? 1 : 0;
+    while (nbr_of_var) {
+        loop_to_replace(str, &next_index, env, spec);
+        nbr_of_var--;
+    }
+
+}
+
+void replace_all_variable(char **env, char **str, char spec)
+{
+    for (int i = 0; str[i]; i++) {
+        if (contain(str[i], '$')) {
+            set_loop_to_replace(&str[i], env, spec);
+        }
+    }
+}
+
 char **split_words(char *input, env_t *vars)
 {
     char **word_parse = NULL;
@@ -319,6 +384,8 @@ char **split_words(char *input, env_t *vars)
     word_parse = str_to_word_array(input, " ");
     delete_all_back_slash(word_parse);
     replace_aliases_in_word_parse(word_parse, vars->aliases->commands);
+    replace_all_variable(vars->vars, word_parse, '\t');
+    replace_all_variable(vars->env, word_parse, '=');
     return (word_parse);
 }
 
