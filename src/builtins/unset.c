c2d9e850e9f5ce0b0205645ab935f628f52cb6d4
Quentin-Desmettre <quentin.desmettre@epitech.eu>
2022-05-28 23:05:11 +0200

    [+] Globings for unset

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -25,6 +25,26 @@ static void rotate_var(char **vars, int start)
     }
 }
 
+char **resolve_unset_globbings(char **args, char **vars)
+{
+    int len = my_str_array_len(vars);
+    char **splitted_vars = malloc(sizeof(char *) * (len + 1));
+    char **tmp;
+    char **solved;
+
+    splitted_vars[len] = NULL;
+    for (int i = 0; vars[i]; i++) {
+        tmp = my_str_to_word_array(vars[i], "\t");
+        splitted_vars[i] = tmp[0];
+        for (int j = 1; tmp[j]; j++)
+            free(tmp[j]);
+        free(tmp);
+    }
+    solved = resolve_globbings(args, splitted_vars);
+    free_str_array(splitted_vars, 0);
+    return solved;
+}
+
 void unsetvar_pipe(char **args, char ***e, int o_fd, int is_pipe)
 {
     int index = o_fd;
@@ -35,11 +55,13 @@ void unsetvar_pipe(char **args, char ***e, int o_fd, int is_pipe)
     set_last_exit(0);
     if (is_pipe)
         return;
-    for (int i = 1; args[i]; i++) {
+    args = resolve_unset_globbings(args, vars);
+    for (int i = 0; args[i]; i++) {
         index = var_index_of_key(vars, args[i]);
         if (index < 0)
             continue;
         free(vars[index]);
         rotate_var(vars, index);
     }
+    free_str_array(args);
 }
